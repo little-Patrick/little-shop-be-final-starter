9438905fe9cefa1f6654595d57d500c558200564
little-Patrick <Little.patrick@outlook.com>
2025-03-09 00:29:54 -0700

    Chore

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Merchant, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:name) }
   end
 
   describe 'relationships' do
