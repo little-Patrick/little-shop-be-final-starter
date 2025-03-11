8bb9a313055487c41ac127c6c4f20622a9c25028
little-Patrick <Little.patrick@outlook.com>
2025-03-11 13:51:14 -0600

    Add: serialization of invoice count

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -2,6 +2,16 @@ class MerchantSerializer
   include JSONAPI::Serializer
   attributes :name
 
+  attributes :coupons_count do |merchant|
+    merchant.coupons.length
+  end
+
+  attributes :invoice_coupon_count do |merchant|
+    merchant.invoices.count do |invoice|
+      invoice.coupon_id
+    end
+  end
+
   attribute :item_count, if: Proc.new { |merchant, params|
     params && params[:count] == true
   } do |merchant|
