47186d3f20514a87a68347ba9fd65449716841ba
little-Patrick <Little.patrick@outlook.com>
2025-03-11 11:33:27 -0600

    Fix: started on the wrong controller

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -1,38 +1,42 @@
-class Api::V1::CouponsController < ApplicationController
-  def index
-    coupons = Coupon.all
-
-    # if params[:sorted].present? && params[:sorted] == "price"
-    #   coupons = Coupon.sort_by_price
-    # end
-
-    render json: CouponSerializer.new(coupons), status: :ok
-  end
-
-  def show
-    coupon = Coupon.find(params[:id])
-    render json: CouponSerializer.new(coupon), status: :ok
-  end
-
-  def create
-    coupon = Coupon.create!(coupon_params) # safe to use create! here because our exception handler will gracefully handle exception
-    render json: CouponSerializer.new(coupon), status: :created
-  end
-
-  def update
-    coupon = Coupon.find(params[:id])
-    # if !coupon_params[:merchant_id].nil?
-    #   merchant = Merchant.find(coupon_params[:merchant_id])
-    # end
-    # coupon.update(coupon_params)
-    # coupon.save
-
-    render json: CouponSerializer.new(coupon), status: :ok
-  end
-
-  private
-
-  def coupon_params
-    params.permit(:name, :code, :percent_off, :dollars_off,:merchant_id, :active)
-  end
-end
+# class Api::V1::Merchants::CouponsController < ApplicationController
+#   def index
+#     coupons = Coupon.all
+#     render json: CouponSerializer.new(coupons), status: :ok
+#   end
+#
+#   def show
+#     coupon = Coupon.find(params[:id])
+#     render json: CouponSerializer.new(coupon), status: :ok
+#   end
+#
+#   # def create
+#   #   merchant = Merchant.find_by(id: coupon_params[:merchant_id])
+#   #   return render json: { error: "Merchant not found" }, status: :not_found unless merchant
+#   #
+#   #   coupon = merchant.coupons.new(coupon_params)
+#   #
+#   #   if coupon.save
+#   #     render json: CouponSerializer.new(coupon), status: :created
+#   #   else
+#   #     render json: { errors: coupon.errors.full_messages }, status: :unprocessable_entity
+#   #   end
+#   # end
+#
+#   def update
+#     coupon = Coupon.find(params[:id])
+#     if params[:status] == "activate"
+#       updated = Coupon.activate_coupon(coupon)
+#     elsif params[:status] == "deactivate"
+#       updated = Coupon.deactivate_coupon(coupon)
+#     else
+#       updated = 
+#     end
+#     render json: CouponSerializer.new(updated), status: :ok
+#   end
+#
+#   private
+#
+#   def coupon_params
+#     params.permit(:name, :code, :percent_off, :dollars_off,:merchant_id, :active)
+#   end
+# end
