8e92531b8b53f312e63373cd00df3258fe900faa
little-Patrick <Little.patrick@outlook.com>
2025-03-11 13:49:24 -0600

    Add: invoice count to merchant

--- app/controllers/api/v1/merchants/invoices_controller.rb
+++ app/controllers/api/v1/merchants/invoices_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::Merchants::InvoicesController < ApplicationController
   def index
     merchant = Merchant.find(params[:merchant_id])
+    
     if params[:status].present?
       invoices = merchant.invoices_filtered_by_status(params[:status])
     else
       invoices = merchant.invoices
     end
-    render json: InvoiceSerializer.new(invoices)
+
+    render json: InvoiceSerializer.new(invoices, include: [:coupon]), status: :ok
   end
 end
