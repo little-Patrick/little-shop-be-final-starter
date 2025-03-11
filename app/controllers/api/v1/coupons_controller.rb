e5515a8ca661d128a7a46d839ac27bf6888e99e6
little-Patrick <Little.patrick@outlook.com>
2025-03-11 13:49:46 -0600

    Add: CRUD routes into controller

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::CouponsController < ApplicationController
   def index
-    coupons = Coupon.all
+    if params[:merchant_id]
+      coupons = Coupon.find_merchants_coupons(params[:merchant_id])
+    elsif params[:merchant_id] && params[:status]
+      coupons = Coupons.filter_merchants_coupons(params[:merchant_id], params[:status])
+    else
+      coupons = Coupon.all
+    end
     render json: CouponSerializer.new(coupons), status: :ok
   end
 
@@ -9,18 +15,26 @@ class Api::V1::CouponsController < ApplicationController
     render json: CouponSerializer.new(coupon), status: :ok
   end
 
-  # def create
-  #   merchant = Merchant.find_by(id: coupon_params[:merchant_id])
-  #   return render json: { error: "Merchant not found" }, status: :not_found unless merchant
-  #
-  #   coupon = merchant.coupons.new(coupon_params)
-  #
-  #   if coupon.save
-  #     render json: CouponSerializer.new(coupon), status: :created
-  #   else
-  #     render json: { errors: coupon.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    merchant = Merchant.find_by(id: coupon_params[:merchant_id])
+    return render json: { error: "Merchant not found" }, status: :not_found unless merchant
+
+    if merchant.coupons.where(active: true).count >= 5
+      return render json: { error: "This merchant already has 5 active coupons" }, status: :unprocessable_entity
+    end
+    if merchant.coupons.exists?(code: coupon_params[:code])
+      return render json: { error: "Coupon code must be unique" }, status: :unprocessable_entity
+    end
+
+    coupon = merchant.coupons.new(coupon_params)
+
+    if coupon.save
+      render json: CouponSerializer.new(coupon), status: :created
+    else
+      render json: { errors: coupon.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def update
     coupon = Coupon.find(params[:id])
 
